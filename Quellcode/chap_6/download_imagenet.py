528ea96ff80c58e9da3f7ca965017d862def8a53
Howdi9 <2bg@posteo.de>
2025-10-26 10:40:52 +0100

    Entfernen der gesamten Historie, "Kapitel 6 Quellcode und Installationsanleitung sowie .gitignore Datei"

--- Quellcode/chap_6/download_imagenet.py
+++
@@ -1,8 +0,0 @@
-import wget
-
-def download_imagenet_classes():
-    res = wget.download('https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json')
-    print(res)
-    #, 'imagenet_class_index.json')
-
-download_imagenet_classes()
